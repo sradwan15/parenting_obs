02879eafc8ec3de07458d9276405950228f31ef7
George <kachergis@stanford.edu>
2019-06-19 18:23:12 -0700

    intro edits

---
+++ paper/cogsci2019/extra_text.Rmd.R
@@ -0,0 +1,50 @@
+unused text
+
+Under such expert guidance, children are encouraged and motivated to engage in more advanced play, undertaking explorations that push the boundaries of what they would be able to do unaided [@Vygotsky1980].
+
+
+What are digital interventions
+With the widespread use of smartphones and tablets worldwide, digitally-delivered interventions could address many of the logistical barriers that have limited scaling up face-to-face delivery methods.
+However, the parent and child outcomes assessed in the review (e.g.infant positive behaviors, satisfaction, emotional symptoms etc.) did not address the nature or quality of parent-infant interactions at a detailed level, for example whether the interventions lead to children paying more attention, or vocabulary changes in parents' language usages. 
+Although digitally-delivered activities are designed to promote learning and cognitive development, it is unclear how they might affect these dimensions of parent-child interactions.
+Thus, we want to conduct this experiment to explore if and how digital scaffolding of activities affect the social and linguistic characteristics of parent-child ineractions.
+The quality of parent-child interactions can be measured by both the social engagement of parents (e.g., joint attention to objects in the environment)[@Bigelow2004] and the quality of language (e.g., vocabulary diversity) [@Malvern2004]. 
+
+
+Communication and message content
+
+
+In our study, we focus on videos that provide suggested play activities to parents. 
+These videos constitute a short-term "guided play" intervention. 
+
+
+Young children spend a large portion of their waking time at play, variously manipulating objects, exploring their environment, and interacting with caregivers and peers.
+Playing with objects allows them to discover hidden object properties and relations, and to build a causal understanding of how objects interact [e.g., @Schulz2007]. 
+Meanwhile, play also gives children an opportunity to set and achieve goals (e.g., build a tower) and to practice a wide range of motor skills (e.g., stacking) that will help them navigate the world [@Singer2006].
+Social play can help children learn about human relationships, both through imitation of adult behaviors and by experiencing and learning to process emotional events such as failures [@Singer2006].
+Of course, young children are rarely playing in isolation: caregivers often provide encouragement and guidance while scaffolding a child's play [@Kaye1970; @Wood1976]. 
+The quality of interactions during such guided play has been shown to influence language learning: parents' joint attention to objects that their child was focused on was positively correlated with the child's subsequent vocabulary growth [@Tomasello1986; @Carpenter1998].
+Episodes of joint attention during guided play have also been found to contain more age-appropriate advanced forms of play [@Bigelow2004].
+More generally, parenting practices early in childhood have been shown to play an important role in shaping the future outcomes of young children [@Hart1995; @Heckman2006]. 
+While interventions often have trouble reaching many parents of very young children, the proliferation of mobile devices offers a good avenut for the digital delivery of parenting advice [@Breitenstein2014].
+However, the efficacy of digital parenting advice has not been widely proven. 
+
+
+## Guided Play Scaffolds Learning
+Children's early play behaviors are often assisted by more skilled and knowledgeable play partners such as their caregivers and older siblings [@Kaye1970]. 
+Under such expert guidance, children are encouraged and motivated to engage in more advanced play, undertaking explorations that push the boundaries of what they would be able to do unaided [@Vygotsky1980].
+These tutorial interactions have been shown to be important components of child development [@Wood1976].
+Thus, with the knowledge of both play and tutorial interactions, guided play, which consists of both active and enjoyable activities as well as close guidance of adults [@Hirsh2008] has drawn researchers' interest. 
+A study of preschoolers showed that guided play scaffolds the environment while still allowing children to maintain a large degree of control, 
+and it outperforms direct-instruction approaches in encouraging a variety of positive academic outcomes [@Weisberg2013].
+Another study found that guided play could facilitate children's vocabulary and comprehensive language development and subsequent literacy skills [@Massey2013].
+
+## Improving Parenting Practices and Language Use
+Thus, including guided play in parenting practices from an early age may boost children's language and educational outcomes. 
+A recent home-visit parenting practices intervention targeting children of low socioeconomic status found that parents in the intervention group gained knowledge of language development, and that this effect sustained four months after the intervention [@Suskind2015].
+However, although a number of interactive measures increased during the experiment, including the number of word tokens, conversational turns, and child vocalizations, these increases did not sustain after the intervention.
+That changes did not sustain could be due to the intervention itself,
+or could merely be that the methods of home-visiting is not sustainable enough for parents to easily and constantly get parenting advice.
+Thus, new methods of delivering parenting advice should be considered.
+
+## Effectiveness of Digital Delivery
